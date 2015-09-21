5acc3608878d58b09a2968be72a805b5618c4bf8
Cuong Dinh <phoboi1@gmail.com>
2015-09-21 14:48:08 -0400

    initial

--- Chapter3p3.java
+++ Chapter3p3.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class Chapter3p3{
 	public static void main (String [] args){
+		String username = "csc200";
 		String password = "Password1";
-		String username, epass;
+
+		String eusername, epass;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your username" );
-		username = scan.nextLine();
+		eusername = scan.nextLine();
 		System.out.println("Enter your password" );
 		epass = scan.nextLine();
 
-		if(epass.equals(password)){
+		if(eusername.equals(username) && epass.equals(password)){
 			System.out.println("Welcome " + username + "!");
 		}else{
 			System.out.println("Wrong password or username!");	
