8e87b3a17aac7e904936e3c914d131e0b0f847ac
Cuong Dinh <phoboi1@gmail.com>
2015-11-04 09:23:35 -0500

    inital

---
+++ CarDriver.java
@@ -0,0 +1,13 @@
+public class CarDriver{
+	public static void main(String[] args){
+		Car car1 = new Car();
+		car1.to_String();
+		
+		Car car2 = new Car("Toyota", 500, 23);
+		car2.to_String();
+
+		Car car3 = new Car("Honda", 250, 45);
+		car3.to_String();
+
+		}
+	}
